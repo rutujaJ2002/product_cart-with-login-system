b4da30fcd95ad952d964f4522b53cbdbc207dfe5
Rutuja Sanjay Jadhav <82172257+rutujaJ2002@users.noreply.github.com>
2022-03-06 16:58:38 +0530

    Update conn.php

--- controller/conn.php
+++ controller/conn.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = "localhost";
-$username = "root";
-$password = "rajashri";
+$username = "username";
+$password = "password";
 $dbname = "productcart";
 
 $conn = mysqli_connect($host,$username,$password,$dbname) or die('Connection Failed!');
